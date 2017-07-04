2c0b6ddcd677435923365ca4d1866c77e41bb169
Alexey Ershov <alex4rom@gmail.com>
2017-07-04 13:14:50 +0300

    Add ex.

---
+++ 476_number_complement.js
@@ -0,0 +1,14 @@
+// var findComplement = function(num) {
+//     if (num === 1) {
+//         return 0;
+//     }
+//     return parseInt(num.toString(2).split('').map(function(n) { return 1 - n }).join(''), 2);
+    
+// };
+
+var findComplement = function(num) {
+    let biNum = num.toString(2);
+	let flippedNum = (~ num) >>> 0;
+    return parseInt(flippedNum.toString(2).slice(- biNum.length), 2);
+    
+};
