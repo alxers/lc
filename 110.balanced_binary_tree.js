6bdc79a8045973b1a57e90f70858d35ba1a6a406
Alexey Ershov <alex4rom@gmail.com>
2017-08-02 18:23:31 +0300

    Add ex.

--- 110.balanced_binary_tree.js
+++ 110.balanced_binary_tree.js
@@ -1,7 +1,15 @@
 var isBalanced = function(root) {
-    if (!root) {
-        return 0;
+    let first, second;
+    function rec(root) {
+        if (!root) {
+            return 0;
+        }
+
+        let left = 1 + rec(root.left)
+        let right = 1 + rec(root.right);
+        return [left, right];
     }
     
-    return Math.abs(isBalanced(root.left) - isBalanced(root.right));
+    [first, second] = rec(root);
+    return Math.abs(first.split(',').length - second.split(',').lentgh) > 1;
 };
