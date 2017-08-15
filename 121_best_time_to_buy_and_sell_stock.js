cd11b8a1d8671d3fb288f7420925bef8289819f8
Alexey Ershov <alex4rom@gmail.com>
2017-08-15 18:19:35 +0300

    Update ex

--- 121_best_time_to_buy_and_sell_stock.js
+++ 121_best_time_to_buy_and_sell_stock.js
@@ -1,15 +1,23 @@
 var maxProfit = function(prices) {
-    let min = 0;
-    let max = 0;
+    let min = prices[0];
+    let max = prices[0];
     let maxDiff = 0;
     let diff = 0;
-    for (let i = 0; i > prices.length; i++) {
+    for (let i = 0; i < prices.length; i++) {
         if (min > prices[i]) {
             min = prices[i];
+            max = 0;
         }
         if (max < prices[i]) {
             max = prices[i];
+            
+            diff = max - min;
+            if (diff > maxDiff) {
+                maxDiff = diff;
+            }
         }
         
     }
+    
+    return maxDiff;
 };
